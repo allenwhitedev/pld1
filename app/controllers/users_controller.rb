9d7dd23c60bacbc4553f07d4e6919cd37b6b5c46
allenwhitedev <allenwhitedev@gmail.com>
2014-10-11 19:24:58 -0400

    Finish signup, organizations and front end tba

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
+  
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Welcome to the Pipeline, you can now check in to events"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+  
   def new
+  	@user = User.new
   end
 
   def show
   	@user = User.find_by(params[:id])
   end
+
+private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+
 end
