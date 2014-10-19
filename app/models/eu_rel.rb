936dda2f1c5ca0432ea71a0e0a32ea6ac2561596
allenwhitedev <allenwhitedev@gmail.com>
2014-10-18 22:56:05 -0400

    Add validations requiring presence of attender and attended in EuRel

--- app/models/eu_rel.rb
+++ app/models/eu_rel.rb
@@ -1,6 +1,6 @@
 class EuRel < ActiveRecord::Base
 	belongs_to :attender, class_name: "User"
 	belongs_to :attended, class_name: "Event"
-	#validates: :attender_id, presence: true
-	#validates: :attended_id, presence: true
+	validates :attender_id, presence: true
+	validates :attended_id, presence: true
 end
