a51f974929bbac7b705547bf344717d4d4f6655a
allenwhitedev <allenwhitedev@gmail.com>
2014-10-22 03:12:06 -0400

    Remove bootstrap.js require from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
